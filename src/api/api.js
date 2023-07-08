ca769a5d1bfbcab1cabcd66072e256763e6c071b
manoj-skyward <manoj@skywardtechno.us>
2023-07-08 15:59:10 +0530

    final_commit

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,3 @@ import axios from 'axios';
 export const API_KEY_STRING = 'apikey=cec566a1';
 
 export const BASE_URL = 'https://www.omdbapi.com/?';
-// const api = axios.create({baseURL: BASE_URL});
-
-// export default api;
